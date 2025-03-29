ce8c96cf57a7d6abce4ebae9b8e504e2d0a8e790
HoHuynhTrung <hohuynhtrung2003@gmail.com>
2025-03-29 16:36:53 +0700

    add icon and fix reponsive

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,17 +6,17 @@ import useContactForm from "@/email/use-contact-form";
 const Contact = () => {
   const {formData, handleChange, handleSubmit, isSending, successMessage} = useContactForm();
   return (
-    <section id="contact" className="mb-28 text-center w-lg scroll-mt-24 py-16">
-      <div className="container mx-auto text-center max-w-6xl">
-        <h2 className="text-4xl font-bold text-center mb-4">Contact Me</h2>
-        <p className="text-sm mb-5">
+    <section id="contact" className="mb-28 text-center w-full scroll-mt-24 py-16 px-4 sm:px-8 lg:px-16">
+      <div className="container mx-auto max-w-4xl sm:max-w-5xl md:max-w-6xl">
+        <h2 className="text-3xl sm:text-4xl font-bold text-center mb-4">Contact Me</h2>
+        <p className="text-sm sm:text-base mb-5">
           Please contact me directly at{" "}
           <a className="text-blue-600" href="mailto:hohuynhtrung2003@gmail.com">
             hohuynhtrung2003@gmail.com
           </a>{" "}
           or contact this form.
         </p>
-        <form onSubmit={handleSubmit} className="space-y-4 rounded-lg p-2 mb-3">
+        <form onSubmit={handleSubmit} className="space-y-4 rounded-lg p-4 sm:p-6 md:p-8 bg-gray-100 shadow-md">
           <input
             type="email"
             name="email"
@@ -28,7 +28,7 @@ const Contact = () => {
             maxLength={500}
           />
           <textarea
-            className="w-full h-50 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-500"
+            className="w-full h-40 sm:h-48 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black-500"
             name="message"
             value={formData.message}
             onChange={handleChange}
@@ -36,7 +36,7 @@ const Contact = () => {
             required
             maxLength={5000}
           />
-          <SubmitBtn isSending={isSending}/>
+          <SubmitBtn isSending={isSending} />
         </form>
         {successMessage && <p className="mt-3 text-green-600">{successMessage}</p>}
       </div>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import portrait from "@/images/chandung.jpg";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
+
 const Hero = () => {
   return (
-    <section id="hero" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-24 py-16">
-
+    <section
+      id="hero"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-24 py-16"
+    >
       <div className="flex items-center justify-center">
         <Image
           src={portrait}
@@ -21,13 +26,27 @@ const Hero = () => {
       </p>
 
       {/* Buttons */}
-      <div className="items-center justify-center mt-4 mb-10  px-10">
+      <div className="flex items-center justify-center mt-4 mb-10  px-10">
         <a
           href="/CV-HoHuynhTrung-GameDeveloper.pdf"
           download
           className="bg-white border border-gray-300 text-gray-800 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100 transition duration-300 shadow-md"
         >
           Download CV
+        </a>
+        <a
+          href="https://www.facebook.com/huynhtrunq.1711"
+          className="p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          target="_blank"
+        >
+          <FaFacebookSquare />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/huynhtrung03/"
+          className="p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          target="_blank"
+        >
+          <BsLinkedin />
         </a>
       </div>
     </section>
